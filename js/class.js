d9308c2c842dc41e59ac4eb756b600bf6972ff70
AbirLegend <57029440+abir-legend@users.noreply.github.com>
2023-03-04 23:15:41 +0530

    Update class.js

--- js/class.js
+++ js/class.js
@@ -1,9 +1,9 @@
-class Listing 
+class Parts 
 {
-    constructor(image, text,link)
+    constructor(name,image,link)
     {
         this.image = image;
-        this.text = text;
+        this.name = name;
         this.link = link;
     }
     open()
@@ -13,4 +13,28 @@ class Listing
             window.open(this.link,'_blank')
         }
     }
+}
+
+class Cpu extends Parts
+{
+    constructor(name,image,link,Uid,ReleaseDate,brand,Igpu,socket,cores,threads,frequency,turboFrequency,Tdp,nm,MemoryMax,l3)
+    {
+        this.image = image;
+        this.name = name;
+        this.link = link;
+        this.Uid = Uid;
+        this.ReleaseDate = ReleaseDate;
+        this.brand = brand;
+        this.Igpu = Igpu;
+        this.socket = socket;
+        this.cores = cores;
+        this.threads = threads;
+        this.frequency = frequency;
+        this.turboFrequency = turboFrequency;
+        this.Tdp = Tdp;
+        this.nm = nm;
+        this.MemoryMax = MemoryMax;
+        this.l3 = l3;
+    }
+    super(name,image,link)
 }
