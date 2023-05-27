1fde1727fc7825230f5dabf0379ef843d7a716af
hrithick <hrithickbiswas6399@gmail.com>
2023-05-27 11:03:37 +0530

    build_page update

--- js/build_page.js
+++ js/build_page.js
@@ -1,3 +1,5 @@
+const db = firebase.database();
+
 function getUrlSearchParams() {
     const searchParams = new URLSearchParams(window.location.search);
 
@@ -14,7 +16,23 @@ const params = getUrlSearchParams();
 // getdataparams(params);
 console.log(params);
 
-params.keys().forEach(element => {
+Object.keys(params).forEach(element => {
     getpartsbyName(element, params[element], function(results) { console.log(results);});
 
 });
+
+function getpartsbyName(type,name,callback) {
+    db.ref(type).orderByChild('name').equalTo(name).once('value')
+      .then(function(snapshot) {
+        // Iterate over the query results
+        snapshot.forEach(function(childSnapshot) {
+          let key = childSnapshot.key;
+          var value = childSnapshot.val();
+          console.log(value);
+          callback(value);
+        });
+      })
+      .catch(function(error) {
+        console.error('Error:', error);
+      });
+  }
