de761235d0b6a7cf2ed9f1161a1bc4874d3ca6dd
hrithick <hrithickbiswas6399@gmail.com>
2023-05-27 10:59:18 +0530

    build page update

--- js/build_page.js
+++ js/build_page.js
@@ -1,1 +1,20 @@
-build_page js starts here...
+function getUrlSearchParams() {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    const params = {};
+  
+    for (const [key, value] of searchParams) {
+      params[key] = value;
+    }
+  
+    return params;
+  }
+
+const params = getUrlSearchParams();
+// getdataparams(params);
+console.log(params);
+
+params.keys().forEach(element => {
+    getpartsbyName(element, params[element], function(results) { console.log(results);});
+
+});
